05fbb1a3cded8348e091c6b120ff06e6defe5aed
cinntian <95114138+cinntian@users.noreply.github.com>
2022-01-07 14:36:41 +0700

    Add files via upload

---
+++ Kesulitan/Mudah5.cpp
@@ -0,0 +1,16 @@
+void Mudah5();
+//Modul ini untuk menentukan tingkat kesulitan yang mudah pada papan 5x5
+//Author : Cintia Ningsih
+//I.S    : Tampilan permainan dengan tingkat kesulitan mudah pada papan 5x5 belum ditampilkan 
+//F.S    : Tampilan permainan dengan tingkat kesulitan mudah pada papan 5x5 sudah ditampilkan
+
+void Mudah5(){
+	while(!game_done())
+	{	
+		
+			LangkahPemain5(1);
+			LangkahKomputerMudah5();		
+		
+	}
+	return;
+}
