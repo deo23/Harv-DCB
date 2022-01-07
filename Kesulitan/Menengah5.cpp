a2e352ba15fad453ec241ea1e5866b03ca788c46
cinntian <95114138+cinntian@users.noreply.github.com>
2022-01-07 14:37:10 +0700

    Add files via upload

---
+++ Kesulitan/Menengah5.cpp
@@ -0,0 +1,16 @@
+void Menengah5();
+//Modul ini untuk menentukan tingkat kesulitan yang menengah pada papan 5x5
+//Author : Cintia Ningsih
+//I.S    : Tampilan permainan dengan tingkat kesulitan menengah pada papan 5x5 belum ditampilkan
+//F.S    : Tampilan permainan dengan tingkat kesulitan menengah pada papan 5x5 sudah ditampilkan
+
+void Menengah5(){
+	while(!game_done())
+	{	
+		
+			LangkahPemain5(1);
+			LangkahKomputerMenengah5();		
+		
+	}
+	return;
+}
