366aa1b3dfd65f73d5d6785d9645f171cd21c4da
cinntian <95114138+cinntian@users.noreply.github.com>
2022-01-07 14:39:00 +0700

    Add files via upload

---
+++ Kesulitan/Menengah9.cpp
@@ -0,0 +1,16 @@
+void Menengah9();
+//Modul ini untuk menentukan tingkat kesulitan yang menengah pada papan 9x9
+//Author : Cintia Ningsih
+//I.S    : Tampilan permainan dengan tingkat kesulitan menengah pada papan 9x9 belum ditampilkan
+//F.S    : Tampilan permainan dengan tingkat kesulitan menengah pada papan 9x9 sudah ditampilkan
+
+void Menengah9(){
+	while(!game_done())
+	{	
+		
+			LangkahPemain9(1);
+			LangkahKomputerMenengah9();		
+		
+	}
+	return;
+}
