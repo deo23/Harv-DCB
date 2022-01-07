31d938b8d6e2fd047d802f3b3c22205bf2545c5b
cinntian <95114138+cinntian@users.noreply.github.com>
2022-01-07 14:37:39 +0700

    Add files via upload

---
+++ Kesulitan/Sulit5.cpp
@@ -0,0 +1,16 @@
+void Sulit5();
+//Modul ini untuk menentukan tingkat kesulitan yang sulit pada papan 5x5
+//Author : Cintia Ningsih
+//I.S    : Tampilan permainan dengan tingkat kesulitan sulit pada papan 5x5 sudah ditampilkan
+//F.S    : Tampilan permainan dengan tingkat kesulitan sulit pada papan 5x5 sudah ditampilkan
+
+void Sulit5(){
+	while(!game_done())
+	{	
+		
+			LangkahPemain5(1);
+			LangkahKomputerSulit5();		
+		
+	}
+	return;
+}
