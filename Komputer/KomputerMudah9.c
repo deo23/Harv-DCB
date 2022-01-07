4e6ae9ab3fe0dc3b35d6fc5c70e94d4f2efca73e
deo23 <95121147+deo23@users.noreply.github.com>
2022-01-07 14:13:37 +0700

    Create KomputerMudah9.c

---
+++ Komputer/KomputerMudah9.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <time.h>
+void KomputerMudah9(){
+    /*
+    Author : Muhammad Deo Audha Rizki
+    
+    Modul untuk menentukan langkah Komputer dengan tingkat kesulitan mudah pada papan 9x9, 
+    dengan mengacak input simbol dan kotak
+    
+
+    I.S : Input simbol dan kotak dari komputer belum ada
+    F.S : Input simbol dan kotak dari komputer sudah ada
+    
+    */
+	
+    int i,move, acaksimbol, baris, kolom;
+    char simbol;
+	time_t t;
+	do{
+   	srand((unsigned) time(&t));
+
+
+    
+    acaksimbol = 1 + rand()% 2;
+    if(acaksimbol == 1){
+        simbol = 'S';
+    }
+    else{
+        simbol = 'O';
+    }
+
+    reset:
+    move = 1 + rand()% 81;
+
+    baris = (move-1)/n;
+	kolom = (move-1)%n;
+
+    if(papan[baris][kolom] != ' '){
+        goto reset;
+    }
+    else{
+        papan[baris][kolom] = simbol;
+    }
+	printf("Komputer memasukkan %c ke kotak %d\n", simbol, move);
+	}while(cek_sos (3, move, simbol));
+}
