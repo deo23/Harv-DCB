4b30cbb8be77bf28a207e43dea0710c1d85fef6f
cinntian <95114138+cinntian@users.noreply.github.com>
2022-01-07 14:41:32 +0700

    Add files via upload

---
+++ Kesulitan/Sulit9.cpp
@@ -0,0 +1,16 @@
+void Sulit9();
+//Modul ini untuk menentukan tingkat kesulitan yang sulit pada papan 9x9
+//Author : Cintia Ningsih
+//I.S    : Tampilan permainan dengan tingkat kesulitan sulit pada papan 9x9 belum ditampilkan
+//F.S    : Tampilan permainan dengan tingkat kesulitan sulit pada papan 9x9 sudah ditampilkan
+
+void Sulit9(){
+	while(!game_done())
+	{	
+		
+			LangkahPemain9(1);
+			LangkahKomputerMenengah9();		
+		
+	}
+	return;
+}
