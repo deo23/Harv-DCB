41015cbdbf895d2299652c7edff42a8887f5f596
deo23 <95121147+deo23@users.noreply.github.com>
2022-01-07 14:18:04 +0700

    Create LangkahKomputerSulit9.c

---
+++ Komputer/LangkahKomputerSulit9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void LangkahKomputerSulit9(){
+    /*
+    Author : Muhammad Deo Audha Rizki
+    
+    Modul untuk menentukan pergerakan Komputer dengan tingkat kesulitan sulit pada papan 9x9
+    
+
+    I.S : Papan belum menampilkan input dari Komputer
+    F.S : Papan menampilkan input dari Komputer
+    
+    */
+	KomputerSulit();
+	board9();
+	return;
+}
