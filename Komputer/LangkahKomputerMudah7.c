a053728d65da819900ec6779ec377a973e294491
deo23 <95121147+deo23@users.noreply.github.com>
2022-01-07 14:15:53 +0700

    Create LangkahKomputerMudah7.c

---
+++ Komputer/LangkahKomputerMudah7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void LangkahKomputerMudah7(){
+    /*
+    Author : Muhammad Deo Audha Rizki
+    
+    Modul untuk menentukan pergerakan Komputer dengan tingkat kesulitan mudah pada papan 7x7
+    
+
+    I.S : Papan belum menampilkan input dari Komputer
+    F.S : Papan menampilkan input dari Komputer
+    
+    */
+	KomputerMudah7();
+	board7();
+	return;
+}
