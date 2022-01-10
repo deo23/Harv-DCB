9c1c96a328ca93aaa0e362869408c31f8081ce80
berlianalfd <95121218+berlianalfd@users.noreply.github.com>
2022-01-10 08:19:18 +0700

    Update pilihlevel.c

--- Menu/pilihlevel.c
+++ Menu/pilihlevel.c
@@ -9,13 +9,20 @@ void pilihLevel(){
     int level;
 
     system("CLS");
-	
-    printf("Pilih Level\n");
-    printf("(1) Mudah\n");
-    printf("(2) Menengah\n");
-    printf("(3) Sulit\n");
-    printf("(0) Menu Utama\n");
-    printf("Masukkan Pilihan :\n");
+    system("color 5");
+    printf("\n\n\n\n\n\n\n\n\n\n");
+    gotoxy(70,12);printf("  _   _   _   _   _   _   _   _   _ \n ");
+    gotoxy(70,13);printf(" | |_| |_| |_| |_| |_| |_| |_| |_| | \n ");
+    gotoxy(70,14);printf(" |                                 | \n");
+    gotoxy(70,15);printf(" |           Pilih Level           |  \n");
+    gotoxy(70,16);printf(" |         (1) Mudah               |   \n");
+    gotoxy(70,17);printf(" |         (2) Menengah            |   \n");
+    gotoxy(70,18);printf(" |         (3) Sulit               |   \n");
+    gotoxy(70,19);printf(" |         (0) Menu Utama          |   \n");
+    gotoxy(70,20);printf(" |                                 |  \n");
+    gotoxy(70,21);printf(" |  _   _   _   _   _   _   _   _  | \n");
+    gotoxy(70,22);printf(" |_| |_| |_| |_| |_| |_| |_| |_| |_|  \n");
+    gotoxy(73,24);printf(" Masukkan Pilihan : ");
     scanf("%d", &level);
 
     switch(level){
